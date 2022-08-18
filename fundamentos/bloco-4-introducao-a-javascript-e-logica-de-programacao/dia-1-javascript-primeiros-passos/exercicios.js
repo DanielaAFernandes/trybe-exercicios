20ce51c25069e4649e03606ffc4b5b9e1e7a8444
Daniela <daniela@pop-os.localdomain>
2022-08-18 19:05:16 -0300

    Exercicio 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios.js
@@ -1,8 +1,17 @@
-const a = 52;
+/*const a = 52;
 const b = 87;
 
 console.log('Soma: ' + (a+b));
 console.log('Subtração: ' + (a-b));
 console.log('Multiplicação: ' + (a*b));
 console.log('Divisão: ' + (a-b));
-console.log('Módulo: ' + (a%b));
+console.log('Módulo: ' + (a%b));*/
+
+const num1 = 14;
+const num2 = 23;
+
+if(num1>num2) {
+    console.log("a é maior que b");
+} else {
+    console.log("b é maior que a");
+}
