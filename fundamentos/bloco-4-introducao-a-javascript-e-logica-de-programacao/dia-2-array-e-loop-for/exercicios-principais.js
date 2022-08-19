817ac6dde296d974cb705b6fa1e0d5281a531d52
Daniela <daniela@pop-os.localdomain>
2022-08-19 15:45:14 -0300

    Exercicio 5

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/exercicios-principais.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/exercicios-principais.js
@@ -10,7 +10,7 @@ for (let index =0; index < numbers.length; index+=1) {
 }
 console.log(result);*/
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+/*let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let result = 0;
 for(let index =0;index <numbers.length; index+=1) {
     result += numbers[index] / 10;
@@ -21,6 +21,21 @@ if(result > 20) {
     console.log("Valor maior que 20");
 } else {
     console.log("Valor menor ou igual a 20");
+}*/
+
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// o valor da variável higherNumber poderia ser qualquer um, desde que fosse menor que o menor número do array numbers. Caso atribuíssemos o valor 101 para a variável, nosso algoritmo estaria errado, pois ele nunca acharia um número maior que 101 no array. Para resolver esse problema vamos iniciar a variavel com o primeiro valor do array. Dessa forma podemos pular a posição 0 e iniciar com index = 1
+
+let higherNumber = numbers[0];
+
+for (let index = 1; index < numbers.length; index += 1) {
+  if (numbers[index] > higherNumber) {
+    higherNumber = numbers[index];
+  }
 }
+
+console.log(higherNumber);
 
 
