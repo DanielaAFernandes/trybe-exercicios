0ad6585051b67e8f5424ef9b2d89bdf18028ebe9
Daniela <daniela@pop-os.localdomain>
2022-08-19 10:12:56 -0300

    Consertei exercicio 5

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios.js
@@ -41,9 +41,17 @@ let anguloA = 60;
 let anguloB = 90;
 let anguloC= 30;
 
-if(anguloA + anguloB + anguloC === 180){
-    console.log("true");
+let sumAngulos = anguloA+anguloB+anguloC;
+let angulosPositivos = anguloA > 0 && anguloB > 0 && anguloC >0;
+
+if(angulosPositivos) {
+    if(sumAngulos === 180){
+        console.log(true);
+    } else {
+        console.log(false);
+    };
 } else {
-    console.log("false");
+    console.log("Erro");
 }
+
 
