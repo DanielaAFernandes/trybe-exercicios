a80de4bd462cc221c6b1021ed86c11f5c7c552a5
Daniela <daniela@pop-os.localdomain>
2022-08-22 16:05:30 -0300

    Exercicio 3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-ealgoritmos/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-ealgoritmos/exercicios.js
@@ -13,11 +13,32 @@ for(let index = 0; index < word.length; index -=1){
     }
 }*/
 
-let word = 'tryber';
+/*let word = 'tryber';
 let reverseWord = '';
 
 reverseWord = word.split('').reverse().join('');
 
-console.log(reverseWord);
+console.log(reverseWord);*/
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let biggerWord = array[0];
+
+for(let index = 0; index < array.length; index +=1){
+    if (array[index].length > biggerWord.length) {
+        biggerWord = array[index];
+    }
+}
+
+console.log(biggerWord);
+    
+let smallerWord = array[0];
+for(let index =0; index <array.length; index +=1){
+    if(array[index].length < smallerWord.length){
+        smallerWord = array[index];
+    }
+}
+
+console.log(smallerWord);
+
 
 
