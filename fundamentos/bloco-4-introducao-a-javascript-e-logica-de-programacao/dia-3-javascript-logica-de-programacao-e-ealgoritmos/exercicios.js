d5cdd06da315f305374e641e9b3449d4e3dd9bf5
Daniela <daniela@pop-os.localdomain>
2022-08-22 16:19:05 -0300

    Adicionei exercicio 4

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-ealgoritmos/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-ealgoritmos/exercicios.js
@@ -20,7 +20,7 @@ reverseWord = word.split('').reverse().join('');
 
 console.log(reverseWord);*/
 
-let array = ['java', 'javascript', 'python', 'html', 'css'];
+/*let array = ['java', 'javascript', 'python', 'html', 'css'];
 let biggerWord = array[0];
 
 for(let index = 0; index < array.length; index +=1){
@@ -38,7 +38,17 @@ for(let index =0; index <array.length; index +=1){
     }
 }
 
-console.log(smallerWord);
+console.log(smallerWord);*/
 
+let numPrimos = [3,5,7,11,13,17,19,23,29,31,37,41,43,47];
+let biggestNumPrimo = numPrimos [0];
+
+for (let index = 1; index < numPrimos.length; index+=1) {
+    if (numPrimos[index]> biggestNumPrimo) {
+        biggestNumPrimo = numPrimos[index];
+    }
+}
+
+console.log(biggestNumPrimo);
 
 
