12cf2069ce53d15caa6f26585384500085314df3
Daniela <daniela@pop-os.localdomain>
2022-08-23 12:16:29 -0300

    Mais um exercicio de fixacao

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
@@ -1,4 +1,4 @@
-let player = {
+/*let player = {
 name: 'Marta',
 lastName: 'Silva',
 age: 34, 
@@ -14,4 +14,16 @@ console.log('A jogadora'+ ' '+ player.name+ ' '+player.lastName+ ' '+ 'foi eleit
 
 console.log('A jogadora'+ ' '+ player.name+ ' '+ player.lastName+ ' '+ 'tem'+ ' '+ player.age+ ' '+ 'anos.');
 
-console.log('A jogadora possui'+' '+ player.medals.golden + ' '+ 'medalhas de ouro e'+ ' '+ player.medals.silver+ ' '+ 'medalhas de prata.');
+console.log('A jogadora possui'+' '+ player.medals.golden + ' '+ 'medalhas de ouro e'+ ' '+ player.medals.silver+ ' '+ 'medalhas de prata.');*/
+
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge',
+  };
+
+  for(let index in names){
+      console.log('Olá'+ ' '+names[index]);
+  };
+
+  
