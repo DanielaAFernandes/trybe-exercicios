7e88ab50f4d6644b9e166f657f372ae941a165b9
Daniela <daniela@pop-os.localdomain>
2022-08-23 18:35:54 -0300

    Novos exercicios

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
@@ -112,8 +112,94 @@ console.log('A jogadora possui'+' '+ player.medals.golden + ' '+ 'medalhas de ou
   
 console.log(leitor.nome+' '+"tem"+ ' '+ leitor.livrosFavoritos.length+ ' '+ "livros favoritos.");*/
 
-      
+
+/*function verificar(word){
+    let reverse = word.split('').reverse().join('');
+    if(reverse === word) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(verificar('ovo'));*/
+
+
+
+
+/*function maiorIndice(array) {
+    let highestNumber =0;
+    for(let index = 1; index < array.length; index+=1){
+        if(array[index]> highestNumber){
+            highestNumber = array[index];
+        }
+    }
+    return highestNumber;
+}
+console.log(maiorIndice([2,5,89,63,10]));*/
+
+/*function menorIndice(array) {
+    let lowestNumber = 10;
+    for(let index = 1; index < array.length; index+=1){
+        if(array[index]< lowestNumber){
+            lowestNumber = array[index];
+        }
+    }
+    return lowestNumber;
+}
+console.log(menorIndice([2,5,89,63,10]));*/
+
+/*function indiceDoMenor(numeros) {
+    let indiceMenor = 0;
+    for (let indice in numeros) {
+      if (numeros[indiceMenor] > numeros[indice]) {
+        indiceMenor = indice;
+      }
+    }
   
+    return indiceMenor;
+  }
+  
+  console.log(indiceDoMenor([2, 4, 6, 7, 10, 0, -3])); //6*/
+
+  /*function indiceDoMaior(numeros) {
+      let indiceMaior = 0;
+      for(let indice in numeros){
+          if(numeros[indiceMaior] < numeros[indice]){
+              indiceMaior = indice;
+          }
+      }
+      return indiceMaior;
+  }
+ 
+  console.log(indiceDoMaior([2,7,58,5]));*/
+
+  /*function maiorPalavra(nomes) {
+      let maiorPalavra = nomes[0];
+      for(let index in nomes){
+          if(maiorPalavra.length < nomes[index].length){
+              maiorPalavra = nomes[index];
+          }
+      }
+      return maiorPalavra;
+      }
+      
+  console.log(maiorPalavra(['Jose', 'Carla','Josefina']));*/
+
+  
+  
+  /*function somatorio(numero) {
+      let total = 0;
+      for(let index = 1; index <= numero; index +=1){
+          total = total + index;
+      }
+    return total;
+  }
+
+  console.log(somatorio(7));*/
+
+  
+ 
 
 
 
