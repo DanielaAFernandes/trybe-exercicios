ee271b8edac24c057484fd6b3cb15bb3bdaddcd2
Daniela <daniela@pop-os.localdomain>
2022-08-23 14:52:31 -0300

    Exercicios 1-5

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
@@ -26,7 +26,7 @@ console.log('A jogadora possui'+' '+ player.medals.golden + ' '+ 'medalhas de ou
       console.log('Olá'+ ' '+names[index]);
   };*/
 
-  let car = {
+  /*let car = {
     model: 'A3 Sedan',
     manufacturer: 'Audi',
     year: 2020
@@ -34,5 +34,56 @@ console.log('A jogadora possui'+' '+ player.medals.golden + ' '+ 'medalhas de ou
 
   for(let index in car){
       console.log(index, car[index]);
+  }*/
+
+  /*let car = {
+      model: 'A3 Sedan',
+      manufacturer: 'Audi',
+      year: 2020
+  };
+
+  for(let value in car){
+      console.log(value, car[value]);
+  };*/
+
+  /*let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+  console.log("Bem-vinda, "+ info.personagem);
+
+  info.recorrente = "Sim";
+
+  console.log(info);
+
+  for(let index in info){
+      console.log(index);
   }
+  
+  for(let index in info){
+      console.log(info[index]);
+  }
+
+  let info2 = {
+      personagem: 'Tio Patinhas',
+      origem: 'Christmas on Bear Mountain',
+      nota: 'O último MacPatinhas',
+      recorrente: 'Sim',
+  };
+
+  for (let properties in info) {
+    if (
+      properties === 'recorrente' &&
+      info[properties] === 'Sim' &&
+      info2[properties] === 'Sim'
+    ) {
+      console.log('Ambos recorrentes');
+    } else {
+      console.log(info[properties] + ' e ' + info2[properties]);
+    }
+  }*/
+
+  
 
